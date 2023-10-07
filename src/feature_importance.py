ad36f6c25121c15924c2bec429673757dca03854
jfinizo <finizolajonnathann@gmail.com>
2023-10-07 11:21:22 -0300

    atualizações finais

--- src/feature_importance.py
+++ src/feature_importance.py
@@ -9,7 +9,6 @@ Created on Sat Oct  7 10:21:10 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 class FeatureImportance:
     
@@ -23,12 +22,10 @@ class FeatureImportance:
         
         rf = RandomForestClassifier()
         rf.fit(self.x_train, self.y_train)
-        predict = rf.predict(self.x_test)
         
         feature_importances = pd.DataFrame(rf.feature_importances_, index=self.x_train.columns, columns=['importance']).sort_values('importance')
         plt.rcParams.update({'font.size': 40}) 
         feature_importances.plot.barh(figsize=(50, 30))
         plt.show()
-        return accuracy_score(self.y_test, predict)
         
         
